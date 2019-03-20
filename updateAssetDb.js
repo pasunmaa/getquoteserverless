3b3e548271cb790856b04d348af520d5f1258432
Petri Asunmaa <petrieas@gmail.com>
2019-03-20 18:34:34 +0200

    Added more test data entriest to db

--- updateAssetDb.js
+++ updateAssetDb.js
@@ -17,15 +17,39 @@ const setNRE1V = docRef.set({
     YahooID: "NRE1V.HE",
   }
 })
-/* 
-const setNRE1V = docRef.set({
-  isin: "FI0009005318",
+
+const docRef2 = db.collection('assets').doc('FI0009000681');
+
+docRef2.set({
+  isin: "FI0009000681",
   basicinfo: {
     exchange: "XHEL",
-    ticker: "NRE1V",
-    fullname: "Nokian Renkaat Oyj",
-    OMXNordicID: "HEX24312",
-    YahooID: "NRE1V.HE",
+    ticker: "NOKIA",
+    fullname: "Nokia Oyj",
+    OMXNordicID: "HEX24311",
+    YahooID: "NOKIA.HE",
   }
 })
-  */ 
+
+const docRef3 = db.collection('assets').doc('FI123456789');
+
+docRef3.set({
+  isin: "FI123456789",
+  basicinfo: {
+    exchange: "XHEL",
+    ticker: "TESTI",
+    fullname: "Testi Oy",
+    OMXNordicID: "HEX12345",
+    YahooID: "TESTI.HE",
+  }
+})
+
+db.collection('assets').get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  })
