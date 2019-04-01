0b5faf4ce1acf139e0d9bd7e0fd72eb7870739d4
Petri Asunmaa <petrieas@gmail.com>
2019-04-01 17:36:35 +0300

    root's index.js contains only requires and exports

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const getHistory = require('./quotes/getHistory')
   Usage: https://us-central1-platinum-form-233310.cloudfunctions.net/secrets
 */
 
-exports.getHistory = async (req, res) => {
+module.exports = getHistory
+
+/* exports.getHistory = async (req, res) => {
     await getHistory.getHistory(req, res)     
     res.end() 
-}
+} */
