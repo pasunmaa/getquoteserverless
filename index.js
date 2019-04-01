5b4d5e90650cde28ce6c325c8b7c680bbe1af30e
Petri Asunmaa <petrieas@gmail.com>
2019-04-01 20:06:05 +0300

    usage comment updated

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const getHistory = require('./quotes/getHistory').getHistory
 const helloWorld = require('./helloworld').helloWorld
 
 /*
-  Usage: https://us-central1-platinum-form-233310.cloudfunctions.net/secrets
+  Usage: https://us-central1-platinum-form-233310.cloudfunctions.net/helloWorld
+  Usage: https://us-central1-platinum-form-233310.cloudfunctions.net/getHistory
 */
 
 module.exports = { getHistory, helloWorld }
